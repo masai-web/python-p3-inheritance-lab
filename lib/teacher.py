8717cbc410100df47b748e04a229e67b6235fe4c
+ <masaitaboi@gmail.com>
2025-05-27 09:46:40 +0300

    g

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,25 @@ from user import User
 
 import random
 
+from random import randint
+from user import User
+
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = self.knowledge.copy()
 
     def teach(self):
-        pass
+        random_index = randint(0, len(self.knowledge) - 1)  
+        return self.knowledge[random_index]
